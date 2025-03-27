e6f3c18bb70bcbe60007c7cf69a3fcefe5fb5930
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-03-27 21:41:53 +0100

    Add WebSocket handler and integrate with Vue component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,31 +2,36 @@ import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify';
 import {initializeDatabase} from './server/utils/mysql';
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: {enabled: true},
-  build: {
-    transpile: ['vuetify'],
-  },
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({autoImport: true}));
-      });
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    build: {
+        transpile: ['vuetify'],
     },
-    '@pinia/nuxt',
-    'pinia-plugin-persistedstate/nuxt',
-  ],
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
+    modules: [
+        (_options, nuxt) => {
+            nuxt.hooks.hook('vite:extendConfig', (config) => {
+                // @ts-expect-error
+                config.plugins.push(vuetify({autoImport: true}));
+            });
+        },
+        '@pinia/nuxt',
+        'pinia-plugin-persistedstate/nuxt',
+    ],
+    nitro: {
+        experimental: {
+            websocket: true
+        }
     },
-  },
-  hooks: {
-    async 'nitro:build:before'() {
-      await initializeDatabase();
+    vite: {
+        vue: {
+            template: {
+                transformAssetUrls,
+            },
+        },
     },
-  },
+    hooks: {
+        async 'nitro:build:before'() {
+            await initializeDatabase();
+        },
+    },
 });
