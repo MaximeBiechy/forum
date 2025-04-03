f60a262bb5642e8179b12051ff1db9b1d3efa3f0
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-04-03 18:13:21 +0200

    Add forum management endpoints and user interface definitions

---
+++ components/LoaderComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-progress-circular indeterminate class="circular-color"></v-progress-circular>
+</template>
+
+<style scoped>
+.circular-color {
+  color: var(--color-primary);
+}
+</style>
