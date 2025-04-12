2c4c856ef528d38b1a9c786fd45b7f70f7b558c0
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-04-12 13:32:09 +0200

    Update French locale settings for relative time in date formatting utility

--- utils/dateUtils.ts
+++ utils/dateUtils.ts
@@ -1,7 +1,27 @@
 import moment from 'moment';
 import 'moment/locale/fr';
 
+// moment.locale('fr'); // ne fonctionne pas
+
+moment.updateLocale('fr', {
+    relativeTime: {
+        future: 'dans %s',
+        past: 'il y a %s',
+        s: 'quelques secondes',
+        ss: '%d secondes',
+        m: 'une minute',
+        mm: '%d minutes',
+        h: 'une heure',
+        hh: '%d heures',
+        d: 'un jour',
+        dd: '%d jours',
+        M: 'un mois',
+        MM: '%d mois',
+        y: 'un an',
+        yy: '%d ans'
+    }
+});
+
 export const formatDate = (date: Date, withoutSuffix = false): string => {
-    moment.locale('fr');
     return moment(date).fromNow(withoutSuffix);
 };
